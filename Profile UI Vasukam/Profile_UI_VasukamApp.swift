88167a36428361e2dbb41732dba1c0ff0154e2f9
Biswamohan-creator <56292855+Biswamohan-creator@users.noreply.github.com>
2023-04-04 11:31:28 +0530

    Initial Commit

---
+++ Profile UI Vasukam/Profile_UI_VasukamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_UI_VasukamApp.swift
+//  Profile UI Vasukam
+//
+//  Created by CHINAM DWARIKANATH PATRA on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Profile_UI_VasukamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
